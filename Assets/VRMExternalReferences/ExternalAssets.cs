c52ba6869248e37e813f595dff1ee41c3d29cab3
Graham Ransom <graham_ransom@hotmail.com>
2022-11-12 09:50:18 +0000

    embedded shaders into dll.

--- Assets/VRMExternalReferences/ExternalAssets.cs
+++ Assets/VRMExternalReferences/ExternalAssets.cs
@@ -1,15 +1,31 @@
 using System;
 using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace ExternalAssets
 {
     public class ExternalAssetsHelper
     {
-        static public void LoadExternalAssets(string strPath)
+        static public void Initialise()
         {
+            AssetBundleCreateRequest shadersBundleCreateRequest = AssetBundle.LoadFromStreamAsync(Assembly.GetExecutingAssembly().GetManifestResourceStream("CombinedVRMDll.Assets.shaders"));
+            AssetBundle assetBundle = shadersBundleCreateRequest.assetBundle;
 
-            AssetBundle myLoadedAssetBundle = AssetBundle.LoadFromFile(strPath);
+            if (!assetBundle)
+            {
+                Debug.LogWarning("Failed to load asset bundle");
+            }
+            else
+            {
+                LoadExternalAssets(assetBundle);
+                assetBundle.Unload(false);
+            }
+        }
+
+        static public void LoadExternalAssets(AssetBundle myLoadedAssetBundle)
+        {
             if (myLoadedAssetBundle != null)
             {
                 var names = myLoadedAssetBundle.GetAllAssetNames();
@@ -20,29 +36,40 @@ namespace ExternalAssets
                     if(obj is Shader)
                     {
                         Shader shader = obj as Shader;
-                        ShaderHelper.AddExternalShader(shader.name, shader);
+                        ShaderHelper.AddExternalShader(shader.name, shader); //function logs which shader is added.
                     }
                     else
                     {
                         Debug.LogWarning($"Ignoring Asset from Assetbundle: {obj.ToString()}.");
                     }
                 }
-            
-                myLoadedAssetBundle.LoadAllAssets();
             }
         }
     }
 
 	public class ShaderHelper
 	{
+        public static Dictionary<String, Shader> m_defaultShaders = null;
         public static Dictionary<String, Shader> m_externalShaders = null;
-        public static void AddExternalShader(String str, Shader shader)
+        public static void AddExternalShader(String str, Shader shader, bool bDefaultShader = false)
         {
-            if (m_externalShaders == null)
-                m_externalShaders = new Dictionary<String, Shader>();
+            if (bDefaultShader)
+            {
+                if (m_defaultShaders == null)
+                    m_defaultShaders = new Dictionary<String, Shader>();
 
-            Debug.LogWarning($"Adding External Shader: {str}.");
-            m_externalShaders.Add(str, shader);
+                Debug.Log($"Adding Default Shader: {str}.");
+                m_defaultShaders[str] = shader; //NOTE: If shader exists by label 'str', it will be replaced.
+
+            }
+            else
+            {
+                if (m_externalShaders == null)
+                    m_externalShaders = new Dictionary<String, Shader>();
+
+                Debug.Log($"Adding External Shader: {str}.");
+                m_externalShaders[str] = shader; //NOTE: If shader exists by label 'str', it will be replaced.
+            }
         }
 
         public static Shader Find(string strShaderName)
@@ -60,6 +87,16 @@ namespace ExternalAssets
             }
             else
                 Debug.Log($"Found Shader in External Shaders: {strShaderName}.");
+
+            if (resultingShader == null && (m_defaultShaders == null || !m_defaultShaders.TryGetValue(strShaderName, out resultingShader)))
+            {
+                if (m_defaultShaders == null)
+                    Debug.Log($"Default Shaders Null.");
+                else
+                    Debug.Log($"Failed to find Shader in Default Shaders: {strShaderName}.");
+            }
+            else
+                Debug.Log($"Found Shader in Default Shaders: {strShaderName}.");
 
             if (resultingShader == null)
             {
